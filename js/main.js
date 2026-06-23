ce65a1a962fb7e67da3b0152170b3a674b9cf981
tolyan28rus <kirichenko28rus@gmail.com>
2026-06-23 11:45:02 +0900

    feat: vk video embed, vote section, donate section

--- js/main.js
+++ js/main.js
@@ -23,14 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
             card.setAttribute('data-aos', 'fade-up');
             card.setAttribute('data-aos-delay', String(100 + i * 100));
             card.innerHTML = `
-                <div class="streamer-avatar">
-                    <i class="fas fa-user"></i>
+                <div class="streamer-embed">
+                    <iframe src="https://live.vkvideo.ru/embed/${s.slug}" allow="autoplay; fullscreen" loading="lazy"></iframe>
                 </div>
                 <div class="streamer-info">
                     <h3>${s.name}</h3>
                     <p>${s.title}</p>
                     <a href="https://live.vkvideo.ru/${s.slug}" class="streamer-link" target="_blank">
-                        <i class="fab fa-vk"></i> VK Видео Live
+                        <i class="fab fa-vk"></i> Открыть на VK Видео
                     </a>
                 </div>
             `;
